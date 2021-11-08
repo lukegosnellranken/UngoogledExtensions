3560aba860b6cb9b2e2fcc67de9deb0f3f39cd69
Luke Gosnell <lukegosnell324@gmail.com>
2021-11-07 21:52:44 -0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+let url1 = "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=";
+let url2 = "&x=id%3D"
+let url3 = "%26installsource%3Dondemand%26uc"
+let version = document.forms[0].versionDropdown.value;
+let extensionID;
+let userURL = document.getElementById("extensionURL").value;
+let finalURL;
+let flag1 = false;
+let flag2 = false;
+let downloadBtn = document.getElementById("downloadBtn");
+
+function onPageLoad() {
+    userURL.value="";
+}
+
+function versionSelect() {
+    version = document.forms[0].versionDropdown.value;
+    console.log(version);
+    flag1 = true;
+    checkflags();
+}
+
+function extensionEntered() {
+    console.log("extendionEntered Hit");
+    userURL = document.getElementById("extensionURL").value;
+    console.log(userURL);
+    if (userURL.length > 10) {
+        flag2 = true;
+        console.log("length check");
+        checkflags();
+    } else {
+        flag2 = false;
+        checkflags();
+    }
+}
+
+function checkflags() {
+    console.log("check");
+    if (flag1 == true && flag2 == true){
+        downloadBtn.disabled = false;
+    } else {
+        downloadBtn.disabled = true;
+    }
+}
+
+function onDownload() {
+    extensionID = userURL;
+    console.log(extensionID);
+    extensionID = extensionID.split("/").pop();
+    console.log(extensionID);
+    finalURL = url1 + version + url2 + extensionID + url3;
+    console.log(finalURL);
+    window.location.href = finalURL;
+}
