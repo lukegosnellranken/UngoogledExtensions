10734ca8076ed75a569846bee6be14020a8f0528
Luke Gosnell <lukegosnell324@gmail.com>
2023-11-21 00:46:25 -0600

    anti-anti-adblock filters for YouTube, new suggested extensions

--- js/script.js
+++ js/script.js
@@ -199,7 +199,40 @@ function get_browser() {
     };
  }
 
+function adblockScript() {
+    const script = `
+        youtube.com##+js(set, yt.config_.openPopupConfig.supportedPopups.adBlockMessageViewModel, false)
 
+        youtube.com##+js(set, Object.prototype.adBlocksFound, 0)
+
+        youtube.com##+js(set, ytplayer.config.args.raw_player_response.adPlacements, [])
+
+        youtube.com##+js(set, Object.prototype.hasAllowedInstreamAd, true)
+
+        ! Youtube
+
+        !#if (!adguard_app_windows && !adguard_app_mac && !adguard_app_android)
+
+        www.youtube.com#%#//scriptlet(‘abort-on-stack-trace’, ‘btoa’, ‘/[A-Za-z]lb \S+polymer/‘)
+
+        ! For the closable popup
+
+        youtube.com#?#ytd-enforcement-message-view-model:has(> div.ytd-enforcement-message-view-model)
+
+        youtube.com#$#tp-yt-iron-overlay-backdrop { display: none !important; }
+
+        youtube.com#$?#tp-yt-paper-dialog:has(> ytd-enforcement-message-view-model > div.ytd-enforcement-message-view-model) { remove: true; }
+
+        ! For the overlay
+
+        youtube.com#$#ytd-watch-flexy[player-unavailable] #player-container-outer.ytd-watch-flexy { visibility: visible !important; }
+
+        youtube.com#$?#yt-playability-error-supported-renderers:has(yt-button-view-model[dialog-dismiss]) { display: none !important; }
+
+        !#endif
+    `
+    navigator.clipboard.writeText(script);
+}
 
 
  /* Outdated functions based on deprecated XMLHttpRequest() functionality.
